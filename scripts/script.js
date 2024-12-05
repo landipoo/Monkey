a0f883b60522b70201aa936c7f0d54769ce426bb
landipoo <landon.mclouth@gmail.com>
2024-12-04 22:17:28 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+﻿
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Website loaded successfully!");
+});
+
+document.querySelectorAll('.close').forEach(button => {
+    button.addEventListener('click', function () {
+       
+        window.location.href = 'gallery.html';
+    });
+});
+
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    const emailField = document.getElementById('email');
+    if (!emailField.value.includes('@')) {
+        alert('Please enter a valid email address.');
+        event.preventDefault(); 
+    }
+});
